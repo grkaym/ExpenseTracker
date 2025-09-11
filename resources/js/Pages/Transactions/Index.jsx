740d0ff7801c1412f09f01ba006ab05cc3da168d
grklina <snowmonkeyspring@gmail.com>
2025-09-11 23:27:52 +0900

    refactor: create transaction table component

--- resources/js/Pages/Transactions/Index.jsx
+++ resources/js/Pages/Transactions/Index.jsx
@@ -5,6 +5,7 @@ import SelectBox from '@/components/SelectBox';
 import Button from '@/components/Button';
 import Field from '@/components/Field';
 import { useMemo, useCallback, useState } from 'react';
+import TransactionTable from '@/components/TransactionTable';
 
 export default function Index({ transactions, categories }) {
   // Get shared props
@@ -70,8 +71,6 @@ export default function Index({ transactions, categories }) {
     });
   }, []);
 
-  const isExpense = (type) => type === 'expense';
-
   return (
     <AuthenticatedLayout>
       <Head title="Transactions" />
@@ -107,54 +106,7 @@ export default function Index({ transactions, categories }) {
         </div>
       </Card>
       <Card className="mt-4">
-        <table className="w-full table-fixed border shadow-sm">
-          <thead>
-            <tr className="bg-slate-100 text-left">
-              <th className="w-32 border-l border-slate-200 px-4 py-2">Date</th>
-              <th className="w-48 border-l border-slate-200 px-4 py-2">
-                Category
-              </th>
-              <th className="w-24 border-l border-slate-200 px-4 py-2">Type</th>
-              <th className="w-64 border-l border-slate-200 px-4 py-2">
-                Amount
-              </th>
-              <th className="truncate border-l border-slate-200 px-4 py-2">
-                Note
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {transactions.map((t, index) => {
-              return (
-                <tr
-                  key={index}
-                  className="border-t odd:bg-white even:bg-gray-50"
-                >
-                  <td className="border-l border-slate-200 px-4 py-2">
-                    {t.date}
-                  </td>
-                  <td className="border-l border-slate-200 px-4 py-2">
-                    {t.category.name}
-                  </td>
-                  <td className="border-l border-slate-200 px-4 py-2">
-                    {t.type}
-                  </td>
-                  <td
-                    className={
-                      `border-l border-slate-200 px-4 py-2 text-right ` +
-                      (isExpense(t.type) ? 'text-red-400' : 'text-blue-400')
-                    }
-                  >
-                    ${t.amount}
-                  </td>
-                  <td className="truncate border-l border-slate-200 px-4 py-2">
-                    {t.note}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <TransactionTable transactions={transactions} />
       </Card>
     </AuthenticatedLayout>
   );
---
+++ resources/js/components/TransactionTable.jsx
@@ -0,0 +1,39 @@
+export default function TransactionTable({ transactions }) {
+  return (
+    <table className="w-full table-fixed border shadow-sm">
+      <thead>
+        <tr className="bg-slate-100 text-left">
+          <th className="w-32 border-l border-slate-200 px-4 py-2">Date</th>
+          <th className="w-48 border-l border-slate-200 px-4 py-2">Category</th>
+          <th className="w-24 border-l border-slate-200 px-4 py-2">Type</th>
+          <th className="w-64 border-l border-slate-200 px-4 py-2">Amount</th>
+          <th className="truncate border-l border-slate-200 px-4 py-2">Note</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map((t, index) => {
+          return (
+            <tr key={index} className="border-t odd:bg-white even:bg-gray-50">
+              <td className="border-l border-slate-200 px-4 py-2">{t.date}</td>
+              <td className="border-l border-slate-200 px-4 py-2">
+                {t.category.name}
+              </td>
+              <td className="border-l border-slate-200 px-4 py-2">{t.type}</td>
+              <td
+                className={
+                  `border-l border-slate-200 px-4 py-2 text-right ` +
+                  (t.type === 'expense' ? 'text-red-400' : 'text-blue-400')
+                }
+              >
+                ${t.amount}
+              </td>
+              <td className="truncate border-l border-slate-200 px-4 py-2">
+                {t.note}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
